0f4abea40c0147ce1e9f3b9bf546840f67052858
Mhd Yooshah <yooshahkk@gmail.com>
2024-08-26 00:43:26 +0530

    first commit

---
+++ script1.js
@@ -0,0 +1,22 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_352mb1s";
+  const templateID = "template_us8df6y";
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Your message sent successfully!!");
+    })
+    .catch((err) => console.log(err));
+}
+document.getElementById("form-group").reset();
